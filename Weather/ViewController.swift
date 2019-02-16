4b07726ed7255efe7064224ce149e2317ad6dfad
Phani <phanindranath.wg113@gmail.com>
2019-02-16 17:15:14 -0600

    Weather app

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CityTextField: UITextField!
+    
+    @IBOutlet weak var WeatherTextView: UITextView!
+    
+    @IBAction func SubmitButton(_ sender: Any) {
+        var city = CityTextField.text!
+        if city.contains(" "){
+            let temp = city.split(separator: " ")
+            city = ""
+            for i in 0 ..< temp.count{
+                city += temp[i]
+                if(i != temp.count - 1){
+                    city += "-"
+                }
+            }
+//            print(city)
+        }
+        let URLString = "https://www.weather-forecast.com/locations/\(city)/forecasts/latest"
+        let url = URL(string: URLString)
+        do{
+            let source = try String(contentsOf: url!)
+            var tokens = source.components(separatedBy: "<span class=\"phrase\">")
+            var data = tokens[1]
+            tokens = data.components(separatedBy: "</span")
+            data = tokens[0]
+            print(data)
+            WeatherTextView.text = data
+            WeatherTextView.isEditable = false
+        }
+        catch{
+            print("Exception occurred")
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
